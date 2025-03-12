623858f5fd5df3ee5f1654b28d7679dc4af72c08
Kaiser-iDusk <palshirsendu03@gmail.com>
2025-03-13 02:15:42 +0530

    simple ray tracing done

--- ray_tracer.cpp
+++ ray_tracer.cpp
@@ -3,27 +3,13 @@
 #include<iostream>
 #include<cmath>
 
-// struct Vec3{
-//     double x, y, z;
-//     Vec3(){x=y=z=0;}
-//     Vec3(double x_, double y_, double z_) {x = x_; y = y_; z = z_;}
-//     Vec3 operator + (const Vec3 b){return Vec3(x+b.x, y+b.y, z+b.z);}
-//     Vec3 operator - (const Vec3 b){return Vec3(x-b.x, y-b.y, z-b.z);}
-//     Vec3 operator * (double c){return Vec3(c*x, x*y, c*z);}
-//     Vec3 operator / (double c){return Vec3(x/c, y/c, z/c);}
-//     // Vec3 operator = (const Vec3 b){return Vec3(b.x, b.y, b.z);}
-//     double dot(const Vec3 b){return (x*b.x + y*b.y + z*b.z);}
-//     double norm(){return pow((x*x + y*y + z*z), 0.5);}
-// };
 
-// struct Ray{
-//     Vec3 o;
-//     Vec3 d;
-//     Ray(Vec3& org, Vec3& dir){
-//         o = org;
-//         d = dir;
-//     }
-// };
+#define WIDTH 600
+#define HEIGHT 512
+#define K_A 0.2
+#define BP_P 6.0
+#define K_D 1.0
+#define K_S 0.9
 
 struct Info{
     Vec3 pt;
@@ -65,26 +51,38 @@ struct Sphere{
 struct Color{
     double r, g, b;
     Color(){r=g=b=0;}
-    Color(double r_, double g_, double b_){r = r_; g = g_; b = b_;}
+    Color(double r_, double g_, double b_){r = _clamp(r_); g = _clamp(g_); b = _clamp(b_);}
+
+    static double _clamp(double x){
+        if(x > 255.0) return 255.0;
+        if(x < 0.0) return 0.0;
+        return x;
+    }
+
+    static Color clamp(Color col){Color ret(_clamp(col.r), _clamp(col.g), _clamp(col.b)); return ret;}
+
+    Color operator+(Color col){Color res(_clamp(r+col.r), _clamp(g+col.g), _clamp(b+col.b)); return res;}
+    Color operator*(double val){Color res(_clamp(r*val), _clamp(g*val), _clamp(b*val)); return res;}
 };
 
 int main(){
-    int W = 540, H = 540;
+    int W = WIDTH, H = HEIGHT;
 
     std::ofstream out("out.ppm");
     out << "P3\n" << W << "\n" << H << "\n" << "255\n";
 
     Color pixel_col[H][W];
     Color white(255, 255, 255);
+    Color dark_gray(5, 5, 5);
     Color red(255, 0, 0);
     Color green(0, 255, 0);
     Color blue(0, 0, 255);
 
     LinAlg solver;
 
-    Vec3 center(W/2, H/2, 150), lgt_src_cen(75, 100, 50);
-    Sphere sphere(center, 60);
-    Sphere light_src(lgt_src_cen, 40);
+    Vec3 center(W/2, H/2, 150), lgt_src_cen(W/2, 0, 50);
+    Sphere sphere(center, 100);
+    Sphere light_src(lgt_src_cen, 1);
 
     for(int y = 0; y< H; y++){
         for(int x = 0; x< W; x++){
@@ -94,19 +92,22 @@ int main(){
 
             Info intersect;
             if(sphere.intersect(ray, &intersect)){ 
-                print(intersect.pt);
-                Vec3 normal = (intersect.pt - center); 
-                Vec3 refl = solver.reflect(dir, normal);
-                Ray reflected(intersect.pt, refl);
+                Vec3 l_dir = (light_src.c - intersect.pt);
+                Vec3 normal = (intersect.pt - center);
+                Vec3 view_dir = dir * (-1.0);
 
                 pixel_col[y][x] = red;
 
-                if(light_src.intersect(reflected, &intersect)){
-                    std::cout << "Intersect\n";
-                    pixel_col[y][x].r = (pixel_col[y][x].r + 255) / 2.0;
-                    pixel_col[y][x].g = (pixel_col[y][x].g + 255) / 2.0;
-                    pixel_col[y][x].b = (pixel_col[y][x].b + 255) / 2.0;
-                }
+                // Bling-Phonng shading
+                Vec3 bisector = (view_dir + l_dir);
+                Vec3 h_dir = bisector / solver.norm(bisector);
+
+                double nl = solver.dot((normal / solver.norm(normal)), (l_dir / solver.norm(l_dir)));
+                double nh = solver.dot((normal / solver.norm(normal)), h_dir);
+                
+                double dt1 = std::max(0.0, nl), dt2 = pow(std::max(0.0, nh), BP_P); 
+
+                pixel_col[y][x] = Color::clamp(red + (dark_gray * K_A) + (white * (K_D * dt1 + K_S * dt2)));
             }
 
             if(light_src.intersect(ray, &intersect)){
@@ -115,9 +116,9 @@ int main(){
                 pixel_col[y][x].b = 255;
             }
 
-            out << pixel_col[y][x].r << "\n";
-            out << pixel_col[y][x].g << "\n";
-            out << pixel_col[y][x].b << "\n";
+            out << (int)pixel_col[y][x].r << "\n";
+            out << (int)pixel_col[y][x].g << "\n";
+            out << (int)pixel_col[y][x].b << "\n";
         }
     }
 
